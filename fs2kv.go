2ce7534d1a583efca7197ad31275e647e93e35f3
yanagitani <takanori.yanagitani@gmail.com>
2022-09-08 13:45:40 +0900

    feat!: removed unused func/var/types...
    BREAKING CHANGE: drop unused func/var/types...

--- fs2kv.go
+++ fs2kv.go
@@ -3,8 +3,6 @@ package fs2kv
 import (
 	"context"
 	"io/fs"
-	"path"
-	"regexp"
 	"unicode/utf8"
 
 	s2k "github.com/takanoriyanagitani/go-sql2keyval"
@@ -15,11 +13,6 @@ type KeyValidator func(key []byte) bool
 type PairValidator func(p s2k.Pair) bool
 
 type BatchValidator func(b s2k.Batch) bool
-
-type FileLike struct {
-	Path string // bucket name + "/" + key(utf8 byte string)
-	Val  []byte
-}
 
 type FileEx interface {
 	File() fs.File // base name only
@@ -50,10 +43,6 @@ func FileExFromStd(raw fs.File) Result[FileEx] {
 	return ResultMap(rnm, nm2ex)
 }
 
-type Batch2FileLike func(b s2k.Batch) s2k.Option[FileLike]
-
-type SetFilelikeBatch func(ctx context.Context, many s2k.Iter[FileLike]) error
-
 type SetFsFileBatch func(ctx context.Context, many s2k.Iter[fs.File]) error
 type SetFilesBatch func(ctx context.Context, many s2k.Iter[FileEx]) error
 type SetFiles func(ctx context.Context, files s2k.Iter[Result[FileEx]]) error
@@ -73,51 +62,7 @@ var BatchIter2fsBuilderUuid func(SetFiles) BatchIter2Fs = batchIter2fsBuilderNew
 
 var Utf8validator KeyValidator = utf8.Valid
 
-func RegexpValidatorNewMust(pat string) KeyValidator {
-	re := regexp.MustCompile(pat)
-	return re.Match
-}
-
-func MultiValidatorNew(v []KeyValidator) KeyValidator {
-	return func(key []byte) bool {
-		var i s2k.Iter[KeyValidator] = s2k.IterFromArray(v)
-		return s2k.IterReduce(i, true, func(state bool, item KeyValidator) bool {
-			invalid := !state
-			if invalid {
-				return false
-			}
-			return item(key)
-		})
-	}
-}
-
-func PairValidatorFromKV(kv KeyValidator) PairValidator {
-	return func(p s2k.Pair) bool {
-		return kv(p.Key)
-	}
-}
-
-func BatchValidatorFromPV(pv PairValidator) BatchValidator {
-	return func(b s2k.Batch) bool {
-		return pv(b.Pair())
-	}
-}
-
 type Bytes2string func(b []byte) s2k.Option[string]
-
-func Batch2FilelikeNew(b2s Bytes2string) Batch2FileLike {
-	return func(b s2k.Batch) s2k.Option[FileLike] {
-		var p s2k.Pair = b.Pair()
-		var k []byte = p.Key
-		var ko s2k.Option[string] = b2s(k)
-		return s2k.OptionMap(ko, func(ks string) FileLike {
-			return FileLike{
-				Path: path.Join(b.Bucket(), ks),
-				Val:  p.Val,
-			}
-		})
-	}
-}
 
 func ErrorWarn(funcError func() error, funcWarn func() error) error {
 	e := funcError()
