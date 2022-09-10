128b435cc00b34f586ab7e8ca363fb32a60e789f
yanagitani <takanori.yanagitani@gmail.com>
2022-09-10 07:33:44 +0000

    test: more coverage

--- db_test.go
+++ db_test.go
@@ -51,5 +51,184 @@ func TestDb(t *testing.T) {
 			var dbNames Result[[]string] = DatabaseListFsEnvDefault(1)(rootdir)
 			checker(t, dbNames.IsOk(), true)
 		})
+
+		t.Run("missing", func(t *testing.T) {
+			t.Parallel()
+
+			var rootdir = filepath.Join(
+				ITEST_FS2KV_DB_ENV_DIR,
+				"missing-instance.d",
+			)
+			var dirname = filepath.Join(
+				ITEST_FS2KV_DB_ENV_DIR,
+				"missing-instance.d",
+				FS2KEYVAL_INSTANCE_NAME,
+			)
+			e := os.RemoveAll(dirname)
+			if nil != e {
+				t.Fatalf("Unable to remove dir: %v", e)
+			}
+
+			var dbNames Result[[]string] = DatabaseListFsEnvDefault(1)(rootdir)
+			checker(t, dbNames.IsOk(), false)
+		})
+
+		t.Run("invalid", func(t *testing.T) {
+			t.Parallel()
+
+			var rootdir = filepath.Join(
+				ITEST_FS2KV_DB_ENV_DIR,
+				"missing-instance.d",
+			)
+			var dirname = filepath.Join(
+				ITEST_FS2KV_DB_ENV_DIR,
+				"missing-instance.d",
+				FS2KEYVAL_INSTANCE_NAME,
+			)
+			e := os.RemoveAll(dirname)
+			if nil != e {
+				t.Fatalf("Unable to remove dir: %v", e)
+			}
+
+			var parentName string = filepath.Dir(dirname)
+			e = os.MkdirAll(parentName, 0755)
+			if nil != e {
+				t.Fatalf("Unable to create parent dir: %v", e)
+			}
+
+			f, e := os.Create(dirname) // regular file
+			if nil != e {
+				t.Fatalf("Unable to create invalid 'directory': %v", e)
+			}
+			defer f.Close()
+
+			var dbNames Result[[]string] = DatabaseListFsEnvDefault(1)(rootdir)
+			checker(t, dbNames.IsOk(), false)
+		})
+
+		t.Run("single database", func(t *testing.T) {
+			t.Parallel()
+
+			var rootdir = filepath.Join(
+				ITEST_FS2KV_DB_ENV_DIR,
+				"singledb-instance.d",
+			)
+			var dirname = filepath.Join(
+				ITEST_FS2KV_DB_ENV_DIR,
+				"singledb-instance.d",
+				FS2KEYVAL_INSTANCE_NAME,
+			)
+			e := initDir(dirname)
+			if nil != e {
+				t.Fatalf("Unable to initialize dir: %v", e)
+			}
+
+			f, e := os.Create(filepath.Join(dirname, "db1.tar"))
+			if nil != e {
+				t.Fatalf("Unable to create dummy database tar file: %v", e)
+			}
+			defer f.Close()
+
+			var dbNames Result[[]string] = DatabaseListFsEnvDefault(1)(rootdir)
+			checker(t, dbNames.IsOk(), true)
+
+			var ss []string = dbNames.Value()
+			checker(t, len(ss), 1)
+
+			checker(t, ss[0], "db1.tar")
+		})
+
+		t.Run("many databases", func(t *testing.T) {
+			t.Parallel()
+
+			var rootdir = filepath.Join(
+				ITEST_FS2KV_DB_ENV_DIR,
+				"manydb-instance.d",
+			)
+			var dirname = filepath.Join(
+				ITEST_FS2KV_DB_ENV_DIR,
+				"manydb-instance.d",
+				FS2KEYVAL_INSTANCE_NAME,
+			)
+			e := initDir(dirname)
+			if nil != e {
+				t.Fatalf("Unable to initialize dir: %v", e)
+			}
+
+			createDummyDatabase := func(dbname string) error {
+				f, e := os.Create(filepath.Join(dirname, dbname))
+				if nil != e {
+					return e
+				}
+				return f.Close()
+			}
+
+			requests := []string{
+				"dummy-db1.tar",
+				"dummy-db2.tar",
+			}
+
+			for _, req := range requests {
+				e = createDummyDatabase(req)
+				if nil != e {
+					t.Fatalf("Unable to create dummy database: %v", e)
+				}
+			}
+
+			var dbNames Result[[]string] = DatabaseListFsEnvDefault(2)(rootdir)
+			checker(t, dbNames.IsOk(), true)
+
+			var ss []string = dbNames.Value()
+			checker(t, len(ss), 2)
+
+			checker(t, ss[0], "dummy-db1.tar")
+			checker(t, ss[1], "dummy-db2.tar")
+		})
+
+		t.Run("too many databases", func(t *testing.T) {
+			t.Parallel()
+
+			var rootdir = filepath.Join(
+				ITEST_FS2KV_DB_ENV_DIR,
+				"toomanydb-instance.d",
+			)
+			var dirname = filepath.Join(
+				ITEST_FS2KV_DB_ENV_DIR,
+				"toomanydb-instance.d",
+				FS2KEYVAL_INSTANCE_NAME,
+			)
+			e := initDir(dirname)
+			if nil != e {
+				t.Fatalf("Unable to initialize dir: %v", e)
+			}
+
+			createDummyDatabase := func(dbname string) error {
+				f, e := os.Create(filepath.Join(dirname, dbname))
+				if nil != e {
+					return e
+				}
+				return f.Close()
+			}
+
+			requests := []string{
+				"dummy-db1.tar",
+				"dummy-db2.tar",
+			}
+
+			for _, req := range requests {
+				e = createDummyDatabase(req)
+				if nil != e {
+					t.Fatalf("Unable to create dummy database: %v", e)
+				}
+			}
+
+			var dbNames Result[[]string] = DatabaseListFsEnvDefault(1)(rootdir)
+			checker(t, dbNames.IsOk(), true)
+
+			var ss []string = dbNames.Value()
+			checker(t, len(ss), 1)
+
+			checker(t, ss[0], "dummy-db1.tar")
+		})
 	})
 }
